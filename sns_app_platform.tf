b842ade6a045a172ade52e3857ebc33ca40bab9f
Martin MV <martin.mv@freshconsulting.com>
2023-03-06 16:44:14 -0600

    SNS-APNS

--- sns_app_platform.tf
+++ sns_app_platform.tf
@@ -1,13 +1,13 @@
-# data "pkcs12_archive" "apns_certificate" {
-#   archive  = filebase64("${path.module}/iOS_apns_cert/APNSSandboxCertificates.p12")
-#   password = var.apns_certificate_password
-# }
-# resource "aws_sns_platform_application" "apns" {
-#   name     = "${local.env}-${local.name}-apns-app"
-#   platform = "APNS_SANDBOX"
-#   platform_principal = data.pkcs12_archive.apns_certificate.certificate
-#   platform_credential = data.pkcs12_archive.apns_certificate.private_key
-# }
+data "pkcs12_archive" "apns_certificate" {
+  archive  = filebase64("${path.module}/iOS_apns_cert/APNSSandboxCertificates.p12")
+  password = var.apns_certificate_password
+}
+resource "aws_sns_platform_application" "apns" {
+  name     = "${local.env}-${local.name}-apns-app"
+  platform = "APNS_SANDBOX"
+  platform_principal = data.pkcs12_archive.apns_certificate.certificate
+  platform_credential = data.pkcs12_archive.apns_certificate.private_key
+}
 
 resource "aws_sns_platform_application" "gcm" {
   name     = "${local.env}-${local.name}-gcm-app"
